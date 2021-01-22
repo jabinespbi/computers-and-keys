ddd414bb74598fb47e9f0a6148bd449a13d52f66
Paul Jabines <jabinespbi@gmail.com>
2021-01-22 20:17:33 +0800

    Avoid using Computer entity as a response

--- src/main/java/pauljabines/exam/isr/computers/ComputerResponse.java
+++ src/main/java/pauljabines/exam/isr/computers/ComputerResponse.java
@@ -1,15 +1,36 @@
 package pauljabines.exam.isr.computers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author Paul Benedict Jabines
  */
 public class ComputerResponse {
-    public Computer computer;
+    public ComputerResponseBody computer;
 
     public static ComputerResponse toComputerResponse(Computer computer) {
         ComputerResponse computerResponse = new ComputerResponse();
-        computerResponse.computer = computer;
+        computerResponse.computer = new ComputerResponseBody();
+        computerResponse.computer.type = computer.getType();
+        computerResponse.computer.maker = computer.getMaker();
+        computerResponse.computer.model = computer.getModel();
+        computerResponse.computer.language = computer.getLanguage();
+
+        // TODO:
+        //  Fix ArrayIndexOutOfBounds when using stream API
+        //  Produces a java.lang.ArrayIndexOutOfBounds due to asm incompatibility to jdk version
+        computerResponse.computer.colors = computer.getColors().stream().map(Color::getName).collect(Collectors.toList());
 
         return computerResponse;
+    }
+
+    public static class ComputerResponseBody {
+        public String type;
+        public String maker;
+        public String model;
+        public String language;
+        public List<String> colors = new ArrayList<>();
     }
 }
